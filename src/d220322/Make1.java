d25de3f26bf1b1b80b06d127d3d974299d1baa69
phj1120 <hyeonj1998@naver.com>
2022-03-22 16:59:58 +0900

    2022.03.22.

---
+++ src/d220322/Make1.java
@@ -0,0 +1,77 @@
+package d220322;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Make1 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[num+1];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[5] = 1;
+
+        int cnt;
+        for (int i = 1; i <= num; i++) {
+            if(dp[i] != 0){
+                continue;
+            }
+            cnt = 300001;
+            if (i % 5 == 0 && dp[i / 5] + 1 < cnt) {
+                cnt = dp[i / 5] + 1;
+            }
+            if (i % 3 == 0 && dp[i / 3] + 1 < cnt) {
+                cnt = dp[i / 3] + 1;
+            }
+            if (i % 2 == 0 && dp[i / 2] + 1 < cnt) {
+                cnt = dp[i / 2] + 1;
+            }
+            if (dp[i - 1 ] + 1 < cnt) {
+                cnt = dp[i - 1] + 1;
+            }
+            dp[i] = cnt;
+        }
+        bw.write(String.valueOf(dp[num]));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+class Make12 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] dp = new int[1000001];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[5] = 1;
+
+        int cnt;
+        for (int i = 1; i <= num; i++) {
+            if(dp[i] != 0){
+                continue;
+            }
+            cnt = 300001;
+            if (i % 5 == 0 && dp[i / 5] + 1 < cnt) {
+                cnt = dp[i / 5] + 1;
+            }
+            if (i % 3 == 0 && dp[i / 3] + 1 < cnt) {
+                cnt = dp[i / 3] + 1;
+            }
+            if (i % 2 == 0 && dp[i / 2] + 1 < cnt) {
+                cnt = dp[i / 2] + 1;
+            }
+            if (dp[i - 1 ] + 1 < cnt) {
+                cnt = dp[i - 1] + 1;
+            }
+            dp[i] = cnt;
+        }
+        System.out.println(dp[num]);
+    }
+}
---
+++ src/d220322/Q1463.java
@@ -0,0 +1,96 @@
+package d220322;
+
+import java.io.*;
+import java.util.Scanner;
+
+// 처음
+public class Q1463 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[num+3];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        int cnt;
+        for (int i = 1; i <= num; i++) {
+            if(dp[i] != 0){
+                continue;
+            }
+            cnt = 1000001;
+
+            if (i % 3 == 0 && dp[i / 3] + 1 < cnt) {
+                cnt = dp[i / 3] + 1;
+            }
+            if (i % 2 == 0 && dp[i / 2] + 1 < cnt) {
+                cnt = dp[i / 2] + 1;
+            }
+            if (dp[i - 1 ] + 1 < cnt) {
+                cnt = dp[i - 1] + 1;
+            }
+            dp[i] = cnt;
+        }
+        bw.write(String.valueOf(dp[num]));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+// 개선 - 실패
+class Q1463_2 {
+    public static int[] dp = new int[1000001];
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        dp[2] = 1;
+        dp[3] = 1;
+
+        int cnt;
+        for (int i = 1; i <= num; i++) {
+            if(dp[i] != 0){
+                continue;
+            }
+            cnt = 1000001;
+            if (i % 3 == 0 && dp[i / 3] + 1 < cnt) {
+                cnt = dp[i / 3] + 1;
+            }
+            if (i % 2 == 0 && dp[i / 2] + 1 < cnt) {
+                cnt = dp[i / 2] + 1;
+            }
+            if (dp[i - 1 ] + 1 < cnt) {
+                cnt = dp[i - 1] + 1;
+            }
+            dp[i] = cnt;
+        }
+        System.out.println(dp[num]);
+    }
+}
+
+// 성공
+class Main {
+
+    public static int[] d = new int[1000001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        for (int i = 2; i <= x; i++) {
+            d[i] = d[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                d[i] = Math.min(d[i], d[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                d[i] = Math.min(d[i], d[i / 3] + 1);
+            }
+        }
+        System.out.println(d[x]);
+    }
+}
