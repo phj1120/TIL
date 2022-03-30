b51198e836efedc6e3665c8445ccdf93b0ff08bf
phj1120 <hyeonj1998@naver.com>
2022-03-30 22:50:53 +0900

    그리디(곱하기 더하기, 모험가)

---
+++ src/d220330/PlusOrMulti.java
@@ -0,0 +1,25 @@
+package d220330;
+
+import java.util.Scanner;
+
+public class PlusOrMulti {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int[] a = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            a[i] = s.charAt(i) - 48;
+        }
+
+        int result = a[0];
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] == 0 || a[i + 1] == 0 || a[i + 1] == 1) {
+                result += a[i + 1];
+            } else {
+                result *= a[i + 1];
+            }
+        }
+        System.out.println(result);
+    }
+}
