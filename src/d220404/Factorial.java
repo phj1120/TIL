c3d4f7e665c0ddae3c67fd4703799b8af5862558
phj1120 <hyeonj1998@naver.com>
2022-04-04 23:05:09 +0900

    DFS, BFS 개념

---
+++ src/d220404/Factorial.java
@@ -0,0 +1,26 @@
+package d220404;
+
+public class Factorial {
+    //    반복적으로 구현한 n!
+    static int factorial_iterative(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 재귀적으로 구현한 n!
+    static int result = 1;
+
+    static int factorial_recursive(int n) {
+        if(n <= 1) return result;
+        return n * factorial_recursive(n - 1);
+    }
+    // 점화식을 그대로 이용하기 때문에 코드가 더 간결함    
+
+    public static void main(String[] args) {
+        System.out.println("반복적으로 구현 : " + factorial_iterative(10));
+        System.out.println("재귀적으로 구현 : " + factorial_recursive(10));
+    }
+}
