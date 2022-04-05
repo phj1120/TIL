73d4e8ae41599cea402a56e5a8b2c8ba67a720e4
phj1120 <hyeonj1998@naver.com>
2022-04-05 12:13:49 +0900

    스택(개념, 후위연산자계산기)

---
+++ y22/m03/d31/InfixToPostfix.java
@@ -0,0 +1,42 @@
+package y22.m03.d31;
+
+import java.util.Scanner;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        String ex = sc.nextLine();
+        String ex = "( 5 + 2 ) * 3";
+        String exp[] = ex.split(" ");
+
+        MyStack s = new MyStack();
+        String result = "";
+        int i = 0;
+        String ch;
+
+        while (exp.length > 0) {
+            ch = exp[i];
+            if ("(".equals(ch)) {
+                s.push(ch);
+            } else if (")".equals(ch)) {
+                result += s.pop();
+                s.pop();
+            } else if ("*".equals(ch) || "/".equals(ch)) {
+                result += s.pop();
+                s.push(ch);
+            } else if ("+".equals(ch) || "-".equals(ch)) {
+                if ("*".equals(s.peek()) || "/".equals(s.peek())) {
+                    s.push(ch);
+                } else {
+                    result += s.pop();
+                    s.push(ch);
+                }
+            } else {
+                result += ch;
+            }
+            i++;
+            System.out.println(s.data);
+        }
+    }
+}
---
+++ y22/m03/d31/PostfixCalc.java
@@ -0,0 +1,41 @@
+package y22.m03.d31;
+
+import java.util.Scanner;
+
+public class PostfixCalc {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String ex = sc.nextLine();
+        String ex = "5 2 * 4 + 7 *";
+        String exp[] = ex.split(" ");
+        MyStack s = new MyStack();
+        int in, x, y = 0;
+        String ch;
+
+        for (int i = 0; i < exp.length; i++) {
+            ch = exp[i];
+            if ("*".equals(ch) || "/".equals(ch) || "+".equals(ch) || "-".equals(ch)) {
+                y = (int) s.pop();
+                x = (int) s.pop();
+                switch (ch) {
+                    case "*":
+                        s.push(y * x);
+                        break;
+                    case "/":
+                        s.push(y / x);
+                        break;
+                    case "+":
+                        s.push(y + x);
+                        break;
+                    case "-":
+                        s.push(y - x);
+                        break;
+                }
+            } else {
+                in = Integer.parseInt(exp[i]);
+                s.push(in);
+            }
+        }
+        System.out.println(s.pop());
+    }
+}
