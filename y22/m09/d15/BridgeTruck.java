d648bd9b6234cc9f3778fa3b71db6d1877e312d8
phj1120 <hyeonj1998@naver.com>
2022-09-15 07:25:57 +0900

    다리를 지나는 트럭(큐)

---
+++ y22/m09/d15/BridgeTruck.java
@@ -0,0 +1,25 @@
+package y22.m09.d15;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> truckQueue = new LinkedList();
+        Queue<Integer> bridgeQueue = new LinkedList();
+
+
+
+        return answer;
+    }
+}
+
+public class BridgeTruck {
+    public static void main(String[] args) {
+
+    }
+}
+
