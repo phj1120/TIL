2452e305ab3fb1eb5253f80744a29e8365aaf0d7
phj1120 <hyeonj1998@naver.com>
2022-09-16 23:36:04 +0900

    다리를 지나는 트럭(큐)

--- y22/m09/d16/BridgeTruck.java
+++ y22/m09/d16/BridgeTruck.java
@@ -1,4 +1,4 @@
-package y22.m09.d15;
+package y22.m09.d16;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -12,27 +12,27 @@ class Solution {
             truckQueue.add(truck);
         }
 
-        for (int i = 0; i < bridge_length - 1; i++) {
+        for (int i = 0; i < bridge_length; i++) {
             bridgeQueue.add(0);
         }
-        Integer firstTruck = truckQueue.poll();
-        bridgeQueue.add(firstTruck);
-        int totalWeight = firstTruck;
-        int time = 1;
-        while (totalWeight != 0) {
-            Integer out = bridgeQueue.poll();
-            if (out == 0) {
-                bridgeQueue.add(0);
-            } else {
-                totalWeight -= out;
-                Integer truck = truckQueue.peek();
-                if (truck != null && totalWeight + truck <= weight) {
-                    truckQueue.poll();
-                    bridgeQueue.add(truck);
-                    totalWeight += truck;
-                }
+
+        int bridgeWeight = 0;
+        int time = 0;
+        while (!truckQueue.isEmpty() || bridgeWeight != 0) {
+            if (truckQueue.isEmpty() && bridgeWeight == 0) {
+                break;
             }
             time++;
+            int out = bridgeQueue.poll();
+            bridgeWeight -= out;
+            if (!truckQueue.isEmpty() && truckQueue.peek() + bridgeWeight <= weight) {
+                int truck = truckQueue.poll();
+                bridgeQueue.offer(truck);
+                bridgeWeight += truck;
+                continue;
+            }
+            bridgeQueue.offer(0);
+
         }
         return time;
     }
@@ -51,4 +51,3 @@ public class BridgeTruck {
         System.out.println(answer);
     }
 }
-
