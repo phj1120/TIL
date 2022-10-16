5f4c36dd4508357dcb8cd2604102380799f2dd39
phj1120 <hyeonj1998@naver.com>
2022-10-17 00:27:15 +0900

    두 큐 합 같게 만들기(2단계) - 완전 탐색 실패

--- y22/m10/d16/README.md
+++ y22/m10/d16/README.md
@@ -4,5 +4,12 @@
 
 [두 큐 합 같게 만들기](https://school.programmers.co.kr/learn/courses/30/lessons/118667)
 
-풀이 중
+![](TIL-64.jpg)
 
+![](TIL-65.jpg)
+
+완전 탐색 테스트 케이스, 일부 데이터 성공
+
+대다수 케이스 시간 초과
+
+음..... 그럼 어떻게 효율적으로 풀어야 될까...
--- y22/m10/d16/SumSameQueue.java
+++ y22/m10/d16/SumSameQueue.java
@@ -1,6 +1,5 @@
 package y22.m10.d16;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -16,23 +15,34 @@ class Solution {
         }
 
         q.offer(new CustomQueue(q1, q2, 0));
-        while (q.isEmpty()) {
+        while (!q.isEmpty()) {
             CustomQueue now = q.poll();
-            int sumQ1 = q1.stream().mapToInt(Integer::intValue).sum();
-            int sumQ2 = q2.stream().mapToInt(Integer::intValue).sum();
+            int sumQ1 = now.q1.stream().mapToInt(Integer::intValue).sum();
+            int sumQ2 = now.q2.stream().mapToInt(Integer::intValue).sum();
+            int newDepth = now.depth + 1;
             if (sumQ1 == sumQ2) {
                 return now.depth;
             }
-            int newDepth = now.depth + 1;
-            Queue<Integer> newQ1_1 = new LinkedList<>(now.q1);
-            Queue<Integer> newQ2_1 = new LinkedList<>(now.q2);
-            newQ2_1.offer(newQ1_1.poll());
-            q.offer(new CustomQueue(newQ1_1, newQ2_1, newDepth));
 
-            Queue<Integer> newQ1_2 = new LinkedList<>(now.q1);
-            Queue<Integer> newQ2_2 = new LinkedList<>(now.q2);
-            newQ1_2.offer(newQ2_2.poll());
-            q.offer(new CustomQueue(newQ1_2, newQ2_2, newDepth));
+            if (!now.q1.isEmpty() || !now.q2.isEmpty()) {
+                Queue<Integer> newQ1_1 = new LinkedList<>(now.q1);
+                Queue<Integer> newQ2_1 = new LinkedList<>(now.q2);
+                if (!newQ1_1.isEmpty() && !newQ2_1.isEmpty()) {
+                    newQ2_1.offer(newQ1_1.poll());
+                    if (!newQ1_1.equals(q1) && !newQ2_1.equals(q2)) {
+                        q.offer(new CustomQueue(newQ1_1, newQ2_1, newDepth));
+                    }
+                }
+
+                Queue<Integer> newQ1_2 = new LinkedList<>(now.q1);
+                Queue<Integer> newQ2_2 = new LinkedList<>(now.q2);
+                if (!newQ1_2.isEmpty() && !newQ2_2.isEmpty()) {
+                    newQ1_2.offer(newQ2_2.poll());
+                    if (!newQ1_2.equals(q1) && !newQ2_2.equals(q2)) {
+                        q.offer(new CustomQueue(newQ1_2, newQ2_2, newDepth));
+                    }
+                }
+            }
         }
         return -1;
     }
@@ -51,5 +61,13 @@ class Solution {
 }
 
 public class SumSameQueue {
-
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] queue1 = {3, 2, 7, 2};
+        int[] queue2 = {4, 6, 5, 1};
+        int correctResult = 2;
+        int result = solution.solution(queue1, queue2);
+        System.out.println(correctResult == result);
+        System.out.println(result);
+    }
 }
