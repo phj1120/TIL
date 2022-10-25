0f479f6b6af8b4aed6bcfa59bc38ffb9a78f48a4
phj1120 <hyeonj1998@naver.com>
2022-10-26 00:31:48 +0900

    LeetCode(Arrays & Hashing) - Longest Consecutive Sequence

--- y22/m10/d25/LongestConsecutiveSequence.java
+++ y22/m10/d25/LongestConsecutiveSequence.java
@@ -1,11 +1,54 @@
 package y22.m10.d25;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
 }
-
 class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        Integer[] numbers = set.toArray(new Integer[0]);
+        Arrays.sort(numbers);
+
+        int longest = 0; // 길이가 0일 경우
+        int count = 1; // 길이가 1일 경우
+        boolean consecutiveState = false;
+
+        for (int i = 0; i< numbers.length; i++){
+            if(i == numbers.length -1){
+                return Math.max(longest, count);
+            }
+
+            // 연속 되는 경우
+            if (numbers[i] + 1 == numbers[i+1]){
+                // 연속 되는 것이 시작 된 경우
+                if (!consecutiveState){
+                    count = 2;
+                    consecutiveState = true;
+                } else{
+                    // 전부터 연속 된 경우
+                    count++;
+                }
+
+                // 연속 되지 않는 경우
+            }else{
+                // 연속된 것이 끊긴 경우
+                if(consecutiveState){
+                    consecutiveState = false;
+                    longest = Math.max(longest, count);
+                }
+            }
+        }
+        return longest;
+    }
+}
+
+class SolutionFail {
     public int longestConsecutive(int[] nums) {
         Arrays.sort(nums);
 
