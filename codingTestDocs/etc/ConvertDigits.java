aa92c4abd310281b381c499e9976f82e7731ab91
phj1120 <hyeonj1998@naver.com>
2022-10-28 12:39:20 +0900

    숫자 각 자리 수 반환하는 메서드 추가

---
+++ codingTestDocs/etc/ConvertDigits.java
@@ -0,0 +1,24 @@
+package codingTestDocs.etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertDigits {
+    public int[] convertDigits(int number) {
+        String str = String.valueOf(number);
+        int[] digits = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            digits[i] = str.charAt(i) - '0';
+        }
+        return digits;
+    }
+
+    public List<Integer> convertDigitsList(int number) {
+        List<Integer> digitList = new ArrayList<>();
+        while (number != 0) {
+            digitList.add(0, number % 10);
+            number /= 10;
+        }
+        return digitList;
+    }
+}
