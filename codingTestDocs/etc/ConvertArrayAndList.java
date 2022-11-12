f75302394d796f7a917ceadf360edd570f032e2b
phj1120 <hyeonj1998@naver.com>
2022-11-13 08:27:43 +0900

    공부 : 배열 <-> 리스트 변환

---
+++ codingTestDocs/etc/ConvertArrayAndList.java
@@ -0,0 +1,25 @@
+package codingTestDocs.etc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConvertArrayAndList {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        // Array -> List
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+
+        // List -> Array
+        // Wrapper 타압
+        Integer[] integerList = list.toArray(new Integer[0]);
+        Integer[] integerList2 = list.toArray(Integer[]::new);
+
+        // 기본형 타입
+//        int[] intList = list.toArray(int[]::new); // 불가능
+//        -> intStream 으로 만들어야 함
+        int[] intArray = list.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}
