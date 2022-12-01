c2d92ffddedaca2fdacf5db92e714b3eb28ed370
phj1120 <hyeonj1998@naver.com>
2022-12-02 00:15:01 +0900

    프로그래머스(구현): K진수에서 소수의 개수 구하기(2단계)

--- y22/m12/d01/KPrimeNumber.java
+++ y22/m12/d01/KPrimeNumber.java
@@ -1,7 +1,6 @@
 package m12.d01;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class KPrimeNumber {
@@ -14,7 +13,6 @@ public class KPrimeNumber {
     }
 
 }
-
 class Solution {
     public int solution(int n, int k) {
         // 진법 변환
@@ -36,24 +34,25 @@ class Solution {
         // 소수 인지 판단
         int count = 0;
         for (long data : datas) {
-            if(data <= 1){
-                continue;
-            }
-            if (data == 2) {
+            if (isPrimeNumber(data)) {
                 count++;
-                continue;
-            }
-            long length = (long)Math.sqrt(data)-1;
-            for (long i = 2; i <= length; i++) {
-                if (data % i == 0) {
-                    break;
-                }
-                if (i == length) {
-                    count++;
-                }
             }
         }
 
         return count;
+    }
+
+    private boolean isPrimeNumber(long data) {
+        if (data <= 1)
+            return false;
+        if (data == 2)
+            return true;
+        long length = (long) Math.sqrt(data) + 1;
+        for (long i = 2; i < length; i++) {
+            if (data % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- y22/m12/d01/README.md
+++ y22/m12/d01/README.md
@@ -1,1 +1,56 @@
 # 2022.12.01.
+
+## k 진수에서 소수의 개수 구하기
+
+[k 진수에서 소수의 개수 구하기](https://school.programmers.co.kr/learn/courses/30/lessons/92335)
+
+![](TIL-128.jpg)
+
+자꾸 이상한 곳에서 안되네... 일단 계속 풀어보자
+
+왜 안됐던거지?
+
+메서드로 뽑았더니 그냥 됐다.
+
+왜 그런지는 잘 모르겠으나, 다른 할게 남아 있어 일단 넘어가고 다음에 봐야겠다.
+
+AS-IS
+
+```
+for (long data : datas) {
+    if(data <= 1){
+        continue;
+    }
+    if (data == 2) {
+        count++;
+        continue;
+    }
+    long length = (long)Math.sqrt(data)-1;
+    for (long i = 2; i <= length; i++) {
+        if (data % i == 0) {
+            break;
+        }
+        if (i == length) {
+            count++;
+        }
+    }
+}
+```
+
+TO-BE
+
+```
+private boolean isPrimeNumber(long data) {
+    if (data <= 1)
+        return false;
+    if (data == 2)
+        return true;
+    long length = (long) Math.sqrt(data) + 1;
+    for (long i = 2; i < length; i++) {
+        if (data % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+```
