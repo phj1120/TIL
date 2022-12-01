1d7bd204748b9d592eb5326aa84c633189f12dc7
phj1120 <hyeonj1998@naver.com>
2022-12-01 23:58:02 +0900

    프로그래머스(구현): K진수에서 소수의 개수 구하기 - 풀이 중

---
+++ y22/m12/d01/KPrimeNumber.java
@@ -0,0 +1,59 @@
+package m12.d01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KPrimeNumber {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int correctAnswer = 3;
+        int answer = solution.solution(437674, 3);
+        System.out.println(correctAnswer == answer);
+        System.out.println(answer);
+    }
+
+}
+
+class Solution {
+    public int solution(int n, int k) {
+        // 진법 변환
+        String changeSystem = "";
+        while (n != 0) {
+            changeSystem = (n % k) + changeSystem;
+            n /= k;
+        }
+        // 자르기
+        String[] splits = changeSystem.split("0");
+        List<Long> datas = new ArrayList<>();
+        for (String split : splits) {
+            if ("".equals(split)) {
+                continue;
+            }
+            datas.add(Long.parseLong(split));
+        }
+
+        // 소수 인지 판단
+        int count = 0;
+        for (long data : datas) {
+            if(data <= 1){
+                continue;
+            }
+            if (data == 2) {
+                count++;
+                continue;
+            }
+            long length = (long)Math.sqrt(data)-1;
+            for (long i = 2; i <= length; i++) {
+                if (data % i == 0) {
+                    break;
+                }
+                if (i == length) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
