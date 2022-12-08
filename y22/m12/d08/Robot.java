a9c3d025a56096151e2a6e15f61c9ef107c953d8
phj1120 <hyeonj1998@naver.com>
2022-12-09 01:10:55 +0900

    [PCCP 모의고사 #2] 실습용 로봇

--- y22/m12/d08/Robot.java
+++ y22/m12/d08/Robot.java
@@ -1,6 +1,7 @@
 package y22.m12.d08;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Robot {
     public static void main(String[] args) {
@@ -14,8 +15,56 @@ public class Robot {
 }
 
 class Solution {
+    List<Character> fourDirection = List.of('N', 'E', 'S', 'W');
+    List<List<Integer>> go = List.of(List.of(0, 1), List.of(1, 0), List.of(0, -1), List.of(-1, 0));
+    List<List<Integer>> back = List.of(List.of(0, -1), List.of(-1, 0), List.of(0, 1), List.of(1, 0));
+
     public int[] solution(String command) {
-        int[] answer = {};
-        return answer;
+        char direction = 'N';
+        int x = 0, y = 0;
+
+        for (char nowCommand : command.toCharArray()) {
+            // 방향 전환 일 경우
+            if (isDirection(nowCommand)) {
+                direction = changeDirection(direction, nowCommand);
+                continue;
+            }
+
+            // 전진 일 경우
+            int nowDirectionIndex = fourDirection.indexOf(direction);
+            if (nowCommand == 'G') {
+                List<Integer> move = go.get(nowDirectionIndex);
+                x += move.get(0);
+                y += move.get(1);
+                continue;
+            }
+
+
+            // 후진 일 경우
+            List<Integer> move = back.get(nowDirectionIndex);
+            x += move.get(0);
+            y += move.get(1);
+        }
+
+        return new int[]{x, y};
+    }
+
+    private char changeDirection(char direction, char command) {
+        if (command == 'R') {
+            return fourDirection.get(linearToCircle(fourDirection.indexOf(direction) + 1, fourDirection.size()));
+        }
+        return fourDirection.get(linearToCircle(fourDirection.indexOf(direction) - 1, fourDirection.size()));
+    }
+
+    private int linearToCircle(int index, int size) {
+        if (index == -1)
+            return size - 1;
+        if (index == size)
+            return 0;
+        return index;
+    }
+
+    private boolean isDirection(char now) {
+        return now == 'R' || now == 'L';
     }
 }
