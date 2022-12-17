023cbc34042fd255a529fd9614dd14e21e2297f7
phj1120 <hyeonj1998@naver.com>
2022-12-18 00:40:34 +0900

    LeetCode(Sliding Window) - 239. Sliding Window Maximum

--- y22/m12/d17/README.md
+++ y22/m12/d17/README.md
@@ -4,6 +4,12 @@
 
 [239. Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/)
 
+슬라이딩 윈도우 이동하면서 각 케이스의 최대값 반환
+
 간단하게 풀어보려고 했는데 시간 복잡도에서 걸렸다.
 
 우선 순위 큐 이용해서 풀어야겠다.
+
+우선 순위 큐로 풀었는데도 시간 초과.
+
+해설 봐야겠다.
--- y22/m12/d17/SlidingWindowMaximum.java
+++ y22/m12/d17/SlidingWindowMaximum.java
@@ -1,19 +1,18 @@
 package m12.d17;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 public class SlidingWindowMaximum {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
-//        int k = 3;
-//        int[] correctAnswer = {3, 3, 5, 5, 6, 7};
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+        int[] correctAnswer = {3, 3, 5, 5, 6, 7};
 
-        int[] nums = {1, -1};
-        int k = 1;
-        int[] correctAnswer = {1, -1};
+//        int[] nums = {1, -1};
+//        int k = 1;
+//        int[] correctAnswer = {1, -1};
         int[] answer = solution.maxSlidingWindow(nums, k);
         System.out.println(Arrays.equals(correctAnswer, answer));
         System.out.println(Arrays.toString(answer));
@@ -21,6 +20,47 @@ public class SlidingWindowMaximum {
 }
 
 class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Node[] nodes = new Node[nums.length];
+        PriorityQueue<Node> pq = new PriorityQueue<>((node1, node2) -> {
+            return node2.value - node1.value;
+        });
+
+        for (int i = 0; i < nums.length; i++) {
+            nodes[i] = new Node(i, nums[i]);
+        }
+
+
+        int[] result = new int[nums.length - k + 1];
+        int left = 0;
+        int right = k - 1;
+        for (int i = 0; i < k; i++) {
+            pq.add(nodes[i]);
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = pq.peek().value;
+            if (i != result.length - 1) {
+                pq.remove(nodes[left++]);
+                pq.add(nodes[++right]);
+            }
+        }
+
+        return result;
+    }
+
+    static class Node {
+        int num;
+        int value;
+
+        public Node(int num, int value) {
+            this.num = num;
+            this.value = value;
+        }
+    }
+}
+
+
+class SolutionFail {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] result = new int[nums.length - k + 1];
         int left = 0;
