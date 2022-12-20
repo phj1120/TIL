cfd7e0f9082439642c1ae327d82d0767e0aeab1f
phj1120 <hyeonj1998@naver.com>
2022-12-21 00:05:40 +0900

    LeetCode(Stack): 150. Evaluate Reverse Polish Notation

--- y22/m12/d20/EvaluateReversePolishNotation.java
+++ y22/m12/d20/EvaluateReversePolishNotation.java
@@ -9,11 +9,12 @@ public class EvaluateReversePolishNotation {
 //        String[] tokens = {"2", "1", "+", "3", "*"};
 //        int correctAnswer = 9;
 
-        String[] tokens = {"4", "13", "5", "/", "+"};
-        int correctAnswer = 6;
+//        String[] tokens = {"4", "13", "5", "/", "+"};
+//        int correctAnswer = 6;
 
-//        String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
-//        int correctAnswer = 22;
+        String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        int correctAnswer = 22;
+
         int answer = solution.evalRPN(tokens);
         System.out.println(answer == correctAnswer);
         System.out.println(answer);
@@ -21,6 +22,37 @@ public class EvaluateReversePolishNotation {
 }
 
 class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> numbers = new Stack<>();
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                Integer num1 = numbers.pop();
+                Integer num2 = numbers.pop();
+                numbers.push(operation(num2, token, num1));
+            } else {
+                numbers.push(Integer.parseInt(token));
+            }
+        }
+
+        return numbers.pop();
+    }
+
+    private Integer operation(int number1, String operator, int number2) {
+        if ("+".equals(operator))
+            return number1 + number2;
+        if ("-".equals(operator))
+            return number1 - number2;
+        if ("*".equals(operator))
+            return number1 * number2;
+        return number1 / number2;
+    }
+
+    private boolean isOperator(String token) {
+        return "*".equals(token) || "/".equals(token) || "+".equals(token) || "-".equals(token);
+    }
+}
+
+class SolutionFirst {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         for (String token : tokens) {
